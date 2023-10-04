2416c756d9f485696fe6b377626ca363626c63dc
Noctalyne <133142273+Noctalyne@users.noreply.github.com>
2023-10-04 23:23:25 +0200

    créa page profil

---
+++ src/Controller/ProfilUtilisateurController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilUtilisateurController extends AbstractController
+{
+    #[Route('/profil/utilisateur', name: 'app_profil_utilisateur')]
+    public function index(): Response
+    {
+        return $this->render('profil_utilisateur/page_profil.html.twig', [
+            'controller_name' => 'ProfilUtilisateurController',
+        ]);
+    }
+}
