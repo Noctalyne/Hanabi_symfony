174103ddbee6ecd47010af1b7682025a2bd00ce0
Noctalyne <133142273+Noctalyne@users.noreply.github.com>
2023-10-07 23:15:49 +0200

    maj police + mise en page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,9 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contact/Formulaire', name: 'app_contact_log')]
+    public function contactAcces(): Response
     {
+
+        return $this->render('contact/contact_log.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function ContactRedirection(): Response
+    {
+
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
         ]);
